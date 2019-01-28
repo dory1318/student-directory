00b6f53c3b4f9f977dece58d47e34e1ff96fbed7
Dora Kemeny <dory1318@gmail.com>
2019-01-28 17:38:03 +0000

    loadfile

--- directory.rb
+++ directory.rb
@@ -1,17 +1,13 @@
-#tudents = [
-  #{name: "Dr. Hannibal Lecter", cohort: :november},
-  #{name: "Darth Vader", cohort: :november},
-  #{name: "Nurse Ratched", cohort: :november},
-  #{name: "Michael Corleone", cohort: :november},
-  #{name: "Alex DeLarge", cohort: :november},
-  #{name: "The Wicked Witch of the West", cohort: :november},
-  #{name: "Terminator", cohort: :november},
-  #{name: "Freddy Krueger", cohort: :november},
-  #{name: "The Joker", cohort: :november},
-  #{name: "Joffrey Baratheon", cohort: :november},
-  #{name: "Norman Bates", cohort: :november}
-#]
 @students = [] #an empty array accessible to all methods
+
+def load_students
+  file = File.open("students.csv","r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
 
 def save_students
   #open the file for writing
@@ -47,6 +43,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -64,6 +61,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
